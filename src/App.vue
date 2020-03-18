6be1aea3ad3c37782af8e4d27bf8d8b427b2b201
Kuznetsov Roman <Roman.I.KUZNETSOV@raiffeisen.ru>
2020-03-18 19:55:39 +0300

    add lobby

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,40 @@
 <template>
     <div id="app">
+        <div class="online" :class="{red: isOffline}">{{status}}</div>
+
         <form
           v-if="!hasLogin"
-          @submit="saveName(name)"
+          @submit.prevent="saveName(lobby, name)"
         >
-            <div class="online">{{status}}</div>
-            <input class="inputName" v-model="name"
-                   required
-                   autofocus
-                   autocomplete="off"
-                   placeholder="name"
-                   maxlength="10"
-            >
+            <label for="lobby">
+                <input class="inputName" v-model="lobby"
+                       required
+                       autocomplete="off"
+                       placeholder="number of lobby"
+                       max="9999"
+                       type="number"
+                       id="lobby"
+                       @input="inputLobby"
+                />
+            </label>
+            <label for="name">
+                <input class="inputName" v-model="name"
+                       required
+                       autofocus
+                       autocomplete="off"
+                       placeholder="yourName"
+                       maxlength="10"
+                       id="name"
+                       @input="inputName"
+                />
+            </label>
+
+            <input type="submit" style="position: absolute; left: -9999px"/>
         </form>
 
         <template v-else>
             <div @click="rename" class="rename">Rename</div>
+            <div class="lobby">Lobby:{{lobby}}</div>
 
             <div class="scores">
                 <div v-for="num in scores"
@@ -27,7 +46,7 @@
             </div>
 
             <div class="user-list">
-                <div v-for="user in users"
+                <div v-for="user in usersInLobby"
                      class="user"
                      :class="{'user__me': user.name === name}"
                 >
@@ -35,8 +54,8 @@
                           @click="removeUser(user.name)"
                           class="clean">X
                     </span>
-                    <div class="user__name" @click="resetScore">{{user.name}}</div>
-                    <div class="user__score" @click="resetScore">
+                    <div class="user__name">{{formattedSession(user.name, 'name')}}</div>
+                    <div class="user__score">
                         <span v-if="isShowScore(user.name)">{{user.score}}</span>
                         <span v-else-if="user.score > 0">V</span>
                     </div>
@@ -52,15 +71,17 @@ const ws = new WebSocket(`ws://${IP}:3000`);
 const defaultClick = 10;
 let idTimeout;
 const SesName = 'SPName';
+const defaultConnection = 'OFFLINE';
 
 export default {
     name: 'App',
     data() {
         return {
             hasLogin: '',
+            lobby: null,
             name: '',
             score: 0,
-            status: 'OFFLINE',
+            status: defaultConnection,
             users: [],
             scores: [0.5, 1, 2, 3, 5, 8, 13, 21, '?'],
             adminMode: false,
@@ -69,7 +90,13 @@ export default {
     },
     computed: {
         isAllVoted() {
-            return !this.users.find(i => !i.score);
+            return !this.usersInLobby.find(i => !i.score);
+        },
+        usersInLobby() {
+            return this.users.filter(i => this.lobby === this.formattedSession(i.name, 'lobby'));
+        },
+        isOffline() {
+            return this.status === defaultConnection;
         },
     },
     methods: {
@@ -86,12 +113,12 @@ export default {
             ws.send(JSON.stringify(Object.assign({name: this.name, score: this.score}, data)));
         },
         clickOnScore(num) {
-            this.score = num;
+            this.score = this.score === num ? 0 : num;
             this.send({command: 'setScore'});
         },
-        saveName(name) {
-            name = name.trim();
-            if (!name || this.findUserByName(name)) {
+        saveName(lobby, name) {
+            name = lobby + '.' + name.trim();
+            if (!lobby || !name || this.findUserByName(name)) {
                 return;
             }
             this.name = name;
@@ -99,24 +126,35 @@ export default {
             sessionStorage.setItem(SesName, this.name);
             this.send({command: 'saveName'});
         },
-        restoreUser() {
-            let name = sessionStorage.getItem(SesName);
-            if (!name) {
+        restoreSession() {
+            let sessionName = sessionStorage.getItem(SesName);
+            if (!sessionName) {
+                return;
+            }
+            let lobby = this.formattedSession(sessionName, 'lobby');
+            let name = this.formattedSession(sessionName, 'name');
+            if (!lobby || !name) {
                 return;
             }
             this.name = name;
+            this.lobby = lobby;
             this.hasLogin = true;
-            this.saveName(name);
+            this.saveName(lobby, name);
             this.send({command: 'saveName'});
+        },
+        formattedSession(user, param) {
+            let splitted = user.split('.');
+            if (param === 'name') {
+                return splitted[1] ? splitted[1] : 'OpsName';
+            } else if (param === 'lobby') {
+                return splitted[0] ? splitted[0] : 'OpsLobby';
+            }
         },
         findUserByName(name) {
             return this.users.find(i => i.name === name);
         },
         removeUser(name) {
             this.send({command: 'removeUser', name});
-        },
-        resetScore() {
-            this.send({command: 'setScore', score: 0});
         },
         adminModeActivator() {
             this.clickToAdminMode--;
@@ -134,6 +172,14 @@ export default {
         isShowScore(name) {
             return this.isAllVoted || name === this.name;
         },
+        inputName() {
+            this.name = this.name.replace(/[^a-zA-Zа-я]+/g, '');
+        },
+        inputLobby() {
+            if (this.lobby.length > 4) {
+                this.lobby = this.lobby.slice(0,4);
+            }
+        },
     },
     created() {
 
@@ -142,10 +188,10 @@ export default {
         };
 
         ws.onopen = () => {
-            this.restoreUser();
-            this.setStatus('online');
+            this.restoreSession();
+            this.setStatus('Online');
         };
-        ws.onclose = () => this.setStatus('stop');
+        ws.onclose = () => this.setStatus(defaultConnection);
 
         document.body.addEventListener('click', this.adminModeActivator);
     }
